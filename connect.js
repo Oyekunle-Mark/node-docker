4d31c6f7b77e09da93ae5c1220d3bd898cd2d386
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-19 19:07:50 +0100

    Used env variable for mongo port

--- connect.js
+++ connect.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-const connection = 'mongodb://localhost:27017/mongo-test';
-
 const connectDb = () =>
-  mongoose.connect(connection, {
+  mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
