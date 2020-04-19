7ea604dc153d1a7fd7834a456db346774a484032
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-19 18:37:32 +0100

    Will hold the db connection file

--- data.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const Languages = new mongoose.Schema({
-  name: String,
-  paradigm: String,
-});
-
-module.exports = mongoose.model('Languages', Languages);
